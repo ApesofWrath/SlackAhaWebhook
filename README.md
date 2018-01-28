982698fe277dfff13cfcdfa0301c6dc171845c7d
codytheking <codytheking93@gmail.com>
2018-01-28 15:28:26 -0800

    fetch Aha! features and post to Slack

---
+++ README.md
@@ -0,0 +1,3 @@
+# Aha! Deadlines Reminders
+
+Python (Python 3) script for checking upcoming deadlines and posting them on the #schedule channel on Slack.
---
+++ reminders.py
@@ -0,0 +1,150 @@
+#! /usr/bin/env python3
+
+# Aha! webhook for Slack
+# Cody King
+# 1/27/2018
+
+
+import sys
+from urllib import request, parse
+import requests, json
+from datetime import date, datetime, timedelta
+import time
+
+
+
+def main():
+    run()
+
+# Check that number of days has been given
+# call method to post message to Slack
+def run():
+    if len(sys.argv) != 2:
+        print('Usage: ./bot.py (time in days)')
+        sys.exit(2)
+
+    else:
+        send_message_to_slack(sys.argv[1])
+        print('Success.')
+
+
+
+# Posting to a Slack channel
+def send_message_to_slack(days):
+
+    # Get upcoming tasks from Aha!
+    slack_message = getFeatures(days)
+
+
+    text = 'Here are the features with deadlines less than {} days away:{}'.format(days, ''.join(slack_message))
+    post = {"text": "{0}".format(text)}
+
+    try:
+        json_data = json.dumps(post)
+
+        req_sched = request.Request(get_url(apes_sched),
+                              data=json_data.encode('ascii'),
+                              headers={'Content-Type': 'application/json'})
+
+        req_gen = request.Request(get_url(apes_gen),
+                              data=json_data.encode('ascii'),
+                              headers={'Content-Type': 'application/json'})
+
+        resp1 = request.urlopen(req_sched)
+        resp2 = request.urlopen(req_gen)
+    except Exception as em:
+        print("EXCEPTION: " + str(em))
+
+
+def getFeatures(days):
+    # Get all features (doesn't have due dates, but we can pull IDs)
+    response_features = requests.get('https://secure.aha.io/api/v1/features/?per_page=100', headers=getHeaders())
+
+    data = response_features.json()
+
+    reference_nums = []
+    names = []
+
+    for i in range(len(data.get('features'))):
+        reference_nums.append(data.get('features')[i].get('reference_num'))
+        names.append(data.get('features')[i].get('name'))
+
+
+    #for i in range(len(names)):
+    #    print('{} ({})'.format(names[i], reference_nums[i]))
+
+    # Get feature with specified reference name
+    #response_indiv = requests.get('https://secure.aha.io/api/v1/features/PU-2', headers=headers)
+
+    due_dates = []
+    release_names = []
+
+    for i in range(len(names)):
+        response_indiv = requests.get('https://secure.aha.io/api/v1/features/{}'.format(reference_nums[i]), headers=getHeaders())
+        data = response_indiv.json()
+        due_dates.append(data.get('feature').get('due_date'))
+        release_names.append(data.get('feature').get('release').get('name'))
+
+    #for i in range(len(names)):
+    #    print('{}, {} ({})'.format(release_names[i], names[i], due_dates[i]))
+
+    slack_message = []
+    for i in range(len(names)):
+        if is_upcoming(due_dates[i], days):
+            slack_message.append('\n{}, {} ({})'.format(release_names[i], names[i], due_dates[i]))
+
+
+    return slack_message
+    # return code
+    #print(response)
+
+    # print all JSON
+    #print(data)
+
+
+def is_upcoming(date, days):
+    vals = date.split('-')
+    today = time.localtime()
+    date_format = "%Y/%m/%d"
+    a = datetime.strptime('{}/{}/{}'.format(vals[0], vals[1], vals[2]), date_format)
+    b = datetime.strptime('{}/{}/{}'.format(today[0], today[1], today[2]), date_format)
+    delta = a - b
+
+    num_days = delta / timedelta (days=1)
+
+    if num_days <= int(days) and num_days >= 0:
+        return True
+
+    else:
+        return False
+
+def get_url(index):
+    with open('stuff.txt') as f:
+        content = f.readlines()
+
+    if index == king_testing:
+        return content[7].rstrip('\n')
+
+    if index == apes_sched:
+        return content[10].rstrip('\n')
+
+    if index == apes_gen:
+        return content[13].rstrip('\n')
+
+    if index == aha_api_key:
+        return content[16].rstrip('\n')
+
+def getHeaders():
+    return {
+        'Authorization': 'Bearer {}'.format(get_url(aha_api_key)',
+        'X-Aha-Account': 'apesofwrath668',
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    }
+
+
+
+
+
+if __name__ == '__main__':
+    main()
